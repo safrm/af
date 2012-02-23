955d2943f80aa19e136190216903d06d667a8bd5
Miroslav Safr <Miroslav.Safr@gmail.com>
2012-02-23 13:11:54 +0200

    first open release

---
+++ af.bat
@@ -0,0 +1,15 @@
+@REM Actual date Folder =  create timestamp dir with optional name linux/windows - https://github.com/safrm/af
+@REM author:  Miroslav Safr miroslav.safr@gmail.com
+
+@echo off
+echo creating actual date directory
+ for /f "tokens=2-4 delims=. " %%g in ('date /t') do ( 
+  set dd=%%g
+  set mm=%%h
+  set yy=%%i
+  set rest=%%j )
+@REM directory
+echo "***************************************************"
+if [%1]==[] ( MD "%yy%_%mm%_%dd%")
+if /i [%1] neq []  ( MD "%yy%_%mm%_%dd%_%1" )
+echo "%yy%_%mm%_%dd_%%1 created"
---
+++ install.sh
@@ -0,0 +1,7 @@
+#/bin/sh
+#Actual date Folder =  create timestamp dir with optional name linux/windows - https://github.com/safrm/af
+#author:  Miroslav Safr miroslav.safr@gmail.com
+BINDIR=/usr/local/bin/
+
+sudo mkdir -p -m 0755 $BINDIR
+sudo install -m 0777 -v ./af  $BINDIR
