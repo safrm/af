59395abbc92bad3bd6c9a0565c8fdedf4a954f34
Miroslav Safr <Miroslav.Safr@gmail.com>
2013-04-10 09:37:08 +0300

    New: ax for actual date text file creation
    Signed-off-by: Miroslav Safr <Miroslav.Safr@gmail.com>

---
+++ ax.bat
@@ -0,0 +1,14 @@
+@REM Actual date tXt file =  create timestamp text file with optional name linux/windows - https://github.com/safrm/af
+@REM author:  Miroslav Safr miroslav.safr@gmail.com
+
+@echo off
+ for /f "tokens=2-4 delims=. " %%g in ('date /t') do ( 
+  set dd=%%g
+  set mm=%%h
+  set yy=%%i
+  set rest=%%j )
+@REM directory
+echo "***************************************************"
+if [%1]==[] ( START notepad "%yy%_%mm%_%dd%.txt" && echo "%yy%_%mm%_%dd%.txt created" )
+if /i [%1] neq []  ( START notepad "%yy%_%mm%_%dd%_%1.txt" && echo "%yy%_%mm%_%dd%_%1.txt created")
+
