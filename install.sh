7e201186f41208ba5f36166f6082d669a57e47b9
Miroslav Safr <Miroslav.Safr@gmail.com>
2013-04-10 10:16:51 +0300

    Fix: installation for ax
    Signed-off-by: Miroslav Safr <Miroslav.Safr@gmail.com>

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #/bin/sh
-#Actual date Folder =  create timestamp dir with optional name linux/windows - https://github.com/safrm/af
+#Actual date Folder =  create timestamp dir or text file  with optional name linux/windows - https://github.com/safrm/af
 #author:  Miroslav Safr miroslav.safr@gmail.com
 BINDIR=/usr/local/bin/
 
 sudo mkdir -p -m 0755 $BINDIR
 sudo install -m 0777 -v ./af  $BINDIR
+sudo install -m 0777 -v ./ax  $BINDIR
