456a55b4ee6e5da091b9bc4f147c1f0cd58cb2d7
Miroslav Safr <Miroslav.Safr@gmail.com>
2014-03-07 13:54:44 +0200

    Fix: launchpad DEB_VERSION_UPSTREAM
    Signed-off-by: Miroslav Safr <Miroslav.Safr@gmail.com>

--- install.sh
+++ install.sh
@@ -4,9 +4,33 @@
 BINDIR=/usr/local/bin/
 MANDIR=/usr/share/man
 
-sudo mkdir -p -m 0755 $BINDIR
-sudo install -m 0777 -v ./af  $BINDIR
-sudo install -m 0777 -v ./ax  $BINDIR
+#root check
+USERID=`id -u`
+[ $USERID -eq "0" ] || {
+    echo "I cannot continue, you should be root or run it with sudo!"
+    exit 0
+}
+
+#automatic version
+if command -v appver 1>/dev/null 2>&1 ; then . appver; else APP_SHORT_VERSION=NA ; APP_FULL_VERSION_TAG=NA ; APP_BUILD_DATE=`date +'%Y%m%d_%H%M'`; fi
+
+#test
+for TEST in $(  grep -r -l -h --exclude-dir=test "#\!/bin/sh" . )
+do
+		sh -n $TEST
+		if  [ $? != 0 ]; then
+			echo "syntax error in $TEST, exiting.." 
+			exit 1
+		fi
+done
+
+#update documentation
+jss-docs-update ./doc 
+
+
+mkdir -p -m 0755 $BINDIR
+install -m 0777 -v ./af  $BINDIR
+install -m 0777 -v ./ax  $BINDIR
 
 MANPAGES=`find ./doc/manpages -type f`
 sudo install -d -m 755 $MANDIR/man1
